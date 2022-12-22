5225136982eae3d973fe96069827ad60eb2effd2
bbookng <bbookng@gmail.com>
2022-12-22 20:24:20 +0900

    스프링부트-JPA 2

--- japshop/japshop/src/main/java/japbook/japshop/domain/Member.java
+++ japshop/japshop/src/main/java/japbook/japshop/domain/Member.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,8 +17,10 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+    @NotEmpty
     private String name;
 
+    @JsonIgnore
     @Embedded
     private Address address;
 
