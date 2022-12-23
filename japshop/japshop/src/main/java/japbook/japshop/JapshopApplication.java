05c228cd9f74decf098590860e40fb586246abc1
bbookng <bbookng@gmail.com>
2022-12-23 19:45:32 +0900

    data-jpa

--- japshop/japshop/src/main/java/japbook/japshop/JapshopApplication.java
+++ japshop/japshop/src/main/java/japbook/japshop/JapshopApplication.java
@@ -1,7 +1,9 @@
 package japbook.japshop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JapshopApplication {
@@ -11,4 +13,10 @@ public class JapshopApplication {
 		SpringApplication.run(JapshopApplication.class, args);
 	}
 
+	@Bean
+	Hibernate5Module hibernate5Module() {
+		Hibernate5Module hibernate5Module = new Hibernate5Module();
+//		hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+		return hibernate5Module;
+	}
 }
